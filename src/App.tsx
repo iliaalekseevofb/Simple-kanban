aab29ff78f713a3bc22952a3c01098c2be62463d
iliaalekseevofb <iliaalekseevofb@gmail.com>
2023-03-18 16:33:10 +0300

    Chakra theme and heading are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Heading, Container } from '@chakra-ui/react';
+
 function App() {
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <>
+      <Heading
+        fontSize={{base: '3xl', sm: '4xl', md: '5xl'}}
+        fontWeight='bold'
+        textAlign='center'
+        bgGradient='linear(to-l, #7928CA, #FF0080)'
+        bgClip='text'
+        mt={4}
+      >
+        Welcome to the Kanban
+      </Heading>
+      <Container
+        maxWidth='container.lg'
+        px={4}
+        py={10}
+      >
+
+      </Container>
+    </>
   )
 }
 
